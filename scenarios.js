82e4aee9859831931c81c50b1076b08527900ce2
David Ortiz <david.ortiz@deployedservices.com>
2024-01-14 09:27:18 -0500

    styling improved

---
+++ scenarios.js
@@ -0,0 +1,350 @@
+const scenarios = {
+    CAT1EXP: {
+        mother: {
+            narratives: [
+                {
+                    cooNarrative:
+                        'The child did stuff',
+                    sponsorNarrative:
+                        'the Sponsor also did stuff',
+                    parentsNarrative:
+                        'the childs parents also did some stuff tooo'
+                },
+                {
+                    cooNarrative:
+                        'The child did stuff',
+                    sponsorNarrative:
+                        'the Sponsor also did stuff',
+                    parentsNarrative:
+                        'the childs parents also did some stuff tooo'
+                },
+                {
+                    cooNarrative:
+                        'The child did stuff',
+                    sponsorNarrative:
+                        'the Sponsor also did stuff',
+                    parentsNarrative:
+                        'the childs parents also did some stuff tooo'
+                }
+            ]
+        },
+        father: {
+            narratives: [
+                {
+                    cooNarrative:
+                        'The child did stuff',
+                    sponsorNarrative:
+                        'the Sponsor also did stuff',
+                    parentsNarrative:
+                        'the childs parents also did some stuff tooo'
+                },
+                {
+                    cooNarrative:
+                        'The child did stuff',
+                    sponsorNarrative:
+                        'the Sponsor also did stuff',
+                    parentsNarrative:
+                        'the childs parents also did some stuff tooo'
+                },
+                {
+                    cooNarrative:
+                        'The child did stuff',
+                    sponsorNarrative:
+                        'the Sponsor also did stuff',
+                    parentsNarrative:
+                        'the childs parents also did some stuff tooo'
+                }
+            ]
+        }
+    },
+    CAT1: {
+        mother: {
+            narratives: [
+                {
+                    cooNarrative:
+                        'The child did stuff',
+                    sponsorNarrative:
+                        'the Sponsor also did stuff',
+                    parentsNarrative:
+                        'the childs parents also did some stuff tooo'
+                },
+                {
+                    cooNarrative:
+                        'The child did stuff',
+                    sponsorNarrative:
+                        'the Sponsor also did stuff',
+                    parentsNarrative:
+                        'the childs parents also did some stuff tooo'
+                },
+                {
+                    cooNarrative:
+                        'The child did stuff',
+                    sponsorNarrative:
+                        'the Sponsor also did stuff',
+                    parentsNarrative:
+                        'the childs parents also did some stuff tooo'
+                }
+            ]
+        },
+        father: {
+            narratives: [
+                {
+                    cooNarrative:
+                        'The child did stuff',
+                    sponsorNarrative:
+                        'the Sponsor also did stuff',
+                    parentsNarrative:
+                        'the childs parents also did some stuff tooo'
+                },
+                {
+                    cooNarrative:
+                        'The child did stuff',
+                    sponsorNarrative:
+                        'the Sponsor also did stuff',
+                    parentsNarrative:
+                        'the childs parents also did some stuff tooo'
+                },
+                {
+                    cooNarrative:
+                        'The child did stuff',
+                    sponsorNarrative:
+                        'the Sponsor also did stuff',
+                    parentsNarrative:
+                        'the childs parents also did some stuff tooo'
+                }
+            ]
+        }
+    },
+    CAT2A: {
+        sister: {
+            narratives: [
+                {
+                    cooNarrative:
+                        'The child did stuff',
+                    sponsorNarrative:
+                        'the Sponsor also did stuff',
+                    parentsNarrative:
+                        'the childs parents also did some stuff tooo'
+                },
+                {
+                    cooNarrative:
+                        'The child did stuff',
+                    sponsorNarrative:
+                        'the Sponsor also did stuff',
+                    parentsNarrative:
+                        'the childs parents also did some stuff tooo'
+                },
+                {
+                    cooNarrative:
+                        'The child did stuff',
+                    sponsorNarrative:
+                        'the Sponsor also did stuff',
+                    parentsNarrative:
+                        'the childs parents also did some stuff tooo'
+                }
+            ]
+        },
+        brother: {
+            narratives: [
+                {
+                    cooNarrative:
+                        'The child did stuff',
+                    sponsorNarrative:
+                        'the Sponsor also did stuff',
+                    parentsNarrative:
+                        'the childs parents also did some stuff tooo'
+                },
+                {
+                    cooNarrative:
+                        'The child did stuff',
+                    sponsorNarrative:
+                        'the Sponsor also did stuff',
+                    parentsNarrative:
+                        'the childs parents also did some stuff tooo'
+                },
+                {
+                    cooNarrative:
+                        'The child did stuff',
+                    sponsorNarrative:
+                        'the Sponsor also did stuff',
+                    parentsNarrative:
+                        'the childs parents also did some stuff tooo'
+                }
+            ]
+        }
+    },
+    CAT2B: {
+        aunt: {
+            narratives: [
+                {
+                    cooNarrative:
+                        'The child did stuff',
+                    sponsorNarrative:
+                        'the Sponsor also did stuff',
+                    parentsNarrative:
+                        'the childs parents also did some stuff tooo'
+                },
+                {
+                    cooNarrative:
+                        'The child did stuff',
+                    sponsorNarrative:
+                        'the Sponsor also did stuff',
+                    parentsNarrative:
+                        'the childs parents also did some stuff tooo'
+                },
+                {
+                    cooNarrative:
+                        'The child did stuff',
+                    sponsorNarrative:
+                        'the Sponsor also did stuff',
+                    parentsNarrative:
+                        'the childs parents also did some stuff tooo'
+                }
+            ]
+        },
+        uncle: {
+            narratives: [
+                {
+                    cooNarrative:
+                        'The child did stuff',
+                    sponsorNarrative:
+                        'the Sponsor also did stuff',
+                    parentsNarrative:
+                        'the childs parents also did some stuff tooo'
+                },
+                {
+                    cooNarrative:
+                        'The child did stuff',
+                    sponsorNarrative:
+                        'the Sponsor also did stuff',
+                    parentsNarrative:
+                        'the childs parents also did some stuff tooo'
+                },
+                {
+                    cooNarrative:
+                        'The child did stuff',
+                    sponsorNarrative:
+                        'the Sponsor also did stuff',
+                    parentsNarrative:
+                        'the childs parents also did some stuff tooo'
+                }
+            ]
+        }
+    },
+    CAT3: {
+        greatAunt: {
+            narratives: [
+                {
+                    cooNarrative:
+                        'The child did stuff',
+                    sponsorNarrative:
+                        'the Sponsor also did stuff',
+                    parentsNarrative:
+                        'the childs parents also did some stuff tooo'
+                },
+                {
+                    cooNarrative:
+                        'The child did stuff',
+                    sponsorNarrative:
+                        'the Sponsor also did stuff',
+                    parentsNarrative:
+                        'the childs parents also did some stuff tooo'
+                },
+                {
+                    cooNarrative:
+                        'The child did stuff',
+                    sponsorNarrative:
+                        'the Sponsor also did stuff',
+                    parentsNarrative:
+                        'the childs parents also did some stuff tooo'
+                }
+            ]
+        },
+        greatUncle: {
+            narratives: [
+                {
+                    cooNarrative:
+                        'The child did stuff',
+                    sponsorNarrative:
+                        'the Sponsor also did stuff',
+                    parentsNarrative:
+                        'the childs parents also did some stuff tooo'
+                },
+                {
+                    cooNarrative:
+                        'The child did stuff',
+                    sponsorNarrative:
+                        'the Sponsor also did stuff',
+                    parentsNarrative:
+                        'the childs parents also did some stuff tooo'
+                },
+                {
+                    cooNarrative:
+                        'The child did stuff',
+                    sponsorNarrative:
+                        'the Sponsor also did stuff',
+                    parentsNarrative:
+                        'the childs parents also did some stuff tooo'
+                }
+            ]
+        }
+    },
+    CAT3UN: {
+        unrelatedMale: {
+            narratives: [
+                {
+                    cooNarrative:
+                        'The child did stuff',
+                    sponsorNarrative:
+                        'the Sponsor also did stuff',
+                    parentsNarrative:
+                        'the childs parents also did some stuff tooo'
+                },
+                {
+                    cooNarrative:
+                        'The child did stuff',
+                    sponsorNarrative:
+                        'the Sponsor also did stuff',
+                    parentsNarrative:
+                        'the childs parents also did some stuff tooo'
+                },
+                {
+                    cooNarrative:
+                        'The child did stuff',
+                    sponsorNarrative:
+                        'the Sponsor also did stuff',
+                    parentsNarrative:
+                        'the childs parents also did some stuff tooo'
+                }
+            ]
+        },
+        unrelatedFemale: {
+            narratives: [
+                {
+                    cooNarrative:
+                        'The child did stuff',
+                    sponsorNarrative:
+                        'the Sponsor also did stuff',
+                    parentsNarrative:
+                        'the childs parents also did some stuff tooo'
+                },
+                {
+                    cooNarrative:
+                        'The child did stuff',
+                    sponsorNarrative:
+                        'the Sponsor also did stuff',
+                    parentsNarrative:
+                        'the childs parents also did some stuff tooo'
+                },
+                {
+                    cooNarrative:
+                        'The child did stuff',
+                    sponsorNarrative:
+                        'the Sponsor also did stuff',
+                    parentsNarrative:
+                        'the childs parents also did some stuff tooo'
+                }
+            ]
+        }
+    }
+}
